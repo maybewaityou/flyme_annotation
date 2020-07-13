12c6a2b56314ad510063d78f6be984e09f050c3e
MeePwn <dong.chunnan@chinamworld.com>
2020-07-13 08:19:52 +0800

    first commit

---
+++ lib/ddd/ddd.dart
@@ -0,0 +1,11 @@
+class DomainInstance {
+  const DomainInstance();
+}
+
+const domainInstance = DomainInstance();
+
+class DomainFactory {
+  const DomainFactory();
+}
+
+const domainFactory = DomainFactory();
--- lib/flyme_annotation.dart
+++ lib/flyme_annotation.dart
@@ -1,5 +1,6 @@
 library flyme_annotation;
 
+export 'ddd/ddd.dart';
+export 'function_widget/function_widget.dart';
 export 'property/property.dart';
 export 'router/router.dart';
-export 'function_widget/function_widget.dart';
